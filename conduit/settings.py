c21daeabbc70d845eae45edc4350676d3d982491
Yazan Farah <yazan93farah@gmail.com>
2025-12-17 00:52:50 +0100

    fix cors problem

--- conduit/settings.py
+++ conduit/settings.py
@@ -136,6 +136,13 @@ CORS_ORIGIN_WHITELIST = (
     'localhost:8282',
 )
 
+CORS_ORIGIN_WHITELIST = tuple(
+    origin.strip()
+    for origin in os.environ.get("CORS_ORIGIN_WHITELIST", "").split(",")
+    if origin.strip()
+)
+
+
 # Tell Django about the custom `User` model we created. The string
 # `authentication.User` tells Django we are referring to the `User` model in
 # the `authentication` module. This module is registered above in a setting
