2dab3b5a5a941f44984920d403319e87d0aa834b
Yazan Farah <yazan93farah@gmail.com>
2025-12-17 01:09:05 +0100

    remove duplicate and add example.env

--- conduit/settings.py
+++ conduit/settings.py
@@ -131,10 +131,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-CORS_ORIGIN_WHITELIST = (
-    '0.0.0.0:8282',
-    'localhost:8282',
-)
 
 CORS_ORIGIN_WHITELIST = tuple(
     origin.strip()
